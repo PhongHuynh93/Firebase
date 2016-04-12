152d73fa12b32e0de7a457fe904dfb8e02466515
Phong <p7157568@gmail.com>
2016-04-12 22:41:02 +0700

    feat - make child + add value

--- app/src/main/java/dhbk/android/firebase/MainActivity.java
+++ app/src/main/java/dhbk/android/firebase/MainActivity.java
@@ -2,15 +2,11 @@ package dhbk.android.firebase;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,27 +23,33 @@ public class MainActivity extends AppCompatActivity {
         buttonSave = (Button) findViewById(R.id.button);
 
         Firebase.setAndroidContext(this);
+
         // kết nối đến con (key)
-        final Firebase myFirebaseRef = new Firebase("https://test-fb-phong093.firebaseio.com/khoapham");
+//        final Firebase myFirebaseRef = new Firebase("https://test-fb-phong093.firebaseio.com/khoapham");
+        final Firebase myFirebaseRef = new Firebase("https://test-fb-phong093.firebaseio.com");
+
         // dò xem (value) trong key trên có thay đổi ko
-        myFirebaseRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                txt.setText(dataSnapshot.getValue().toString());
-            }
+//        myFirebaseRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                txt.setText(dataSnapshot.getValue().toString());
+//            }
+//
+//            @Override
+//            public void onCancelled(FirebaseError firebaseError) {
+//
+//            }
+//        });
+//
+//        buttonSave.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // push to database
+//                myFirebaseRef.setValue(editText.getText().toString());
+//            }
+//        });
 
-            @Override
-            public void onCancelled(FirebaseError firebaseError) {
-
-            }
-        });
-
-        buttonSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // push to database
-                myFirebaseRef.setValue(editText.getText().toString());
-            }
-        });
+        // tao child branch + value
+        myFirebaseRef.child("message").setValue("you'll love firebases");
     }
 }
