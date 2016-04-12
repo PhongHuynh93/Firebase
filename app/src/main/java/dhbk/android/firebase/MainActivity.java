52cd1484e5b262f190ae9e54ba976b76e96c1c0a
Phong <p7157568@gmail.com>
2016-04-12 13:53:44 +0700

    feat - add push value to db
    and pull value to every device

--- app/src/main/java/dhbk/android/firebase/MainActivity.java
+++ app/src/main/java/dhbk/android/firebase/MainActivity.java
@@ -2,6 +2,9 @@ package dhbk.android.firebase;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.firebase.client.DataSnapshot;
@@ -12,16 +15,20 @@ import com.firebase.client.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
     private TextView txt;
+    private EditText editText;
+    private Button buttonSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt = (TextView) findViewById(R.id.testFB);
+        txt = (TextView) findViewById(R.id.testFBTextView);
+        editText = (EditText) findViewById(R.id.testFBEditText);
+        buttonSave = (Button) findViewById(R.id.button);
 
         Firebase.setAndroidContext(this);
         // kết nối đến con (key)
-        Firebase myFirebaseRef = new Firebase("https://test-fb-phong093.firebaseio.com/khoapham");
+        final Firebase myFirebaseRef = new Firebase("https://test-fb-phong093.firebaseio.com/khoapham");
         // dò xem (value) trong key trên có thay đổi ko
         myFirebaseRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -32,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCancelled(FirebaseError firebaseError) {
 
+            }
+        });
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // push to database
+                myFirebaseRef.setValue(editText.getText().toString());
             }
         });
     }
